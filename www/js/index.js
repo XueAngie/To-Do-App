16eac77d45bdde172b2c45fa4e48ec675123a5f3
Xue <6681@ait.nsw.edu.au>
2019-05-06 01:01:13 +1000

    Implemented delete function
    Deletes items from local storage

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,11 @@
+var deleteIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+
 var addElement = document.querySelector('.addBtn');
 var inputValue = document.getElementById("myInput");
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
+    todo: [],
+};
+renderList();
 var app = {
     // Application Constructor
     initialize: function() {
@@ -18,7 +24,6 @@ var app = {
 
 app.initialize();
 
-
 //Add item
 addElement.addEventListener('click', function() {
     var value = document.getElementById("myInput").value;
@@ -28,31 +33,41 @@ addElement.addEventListener('click', function() {
 
 
 function addList(value) {
-    console.log(value);
     newElement(value)
+    document.getElementById('myInput').value = '';
+    data.todo.push(value);
+    dataObjectUpdated();
 }
 
+function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+    if (id === 'myUL') {
+        data.todo.splice(data.todo.indexOf(value), 1);
+    }
+    dataObjectUpdated();
+
+    parent.removeChild(item);
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
+function renderList() {
+    //Return nothing if empty
+    if (!data.todo.length) return;
+
+    for (var i = 0; i < data.todo.length; i++) {
+        var value = data.todo[i];
+        newElement(value);
     }
 }
+
+
+function dataObjectUpdated() {
+    localStorage.setItem('todoList', JSON.stringify(data));
+}
+
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
@@ -62,26 +77,24 @@ list.addEventListener('click', function(ev) {
     }
 }, false);
 
-function newElement(item) {
-    var li = document.createElement("li");
-    var t = document.createTextNode(item);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+function newElement(text) {
+    var list = document.getElementById('myUL');
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+    var item = document.createElement('li');
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = deleteIcon;
+
+    // Add click event for removing the item
+    remove.addEventListener('click', removeItem);
+
+    buttons.appendChild(remove);
+    item.appendChild(buttons);
+
+    list.insertBefore(item, list.childNodes[0]);
 }
